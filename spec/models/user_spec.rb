eb2799e129dcc56a5f5a530b8fd410d847fdbc27
Cihangir Okuyan <chiryoku@gmail.com>
2015-01-07 13:24:58 -0500

    Add user model method specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 RSpec.describe User, :type => :model do
   describe "validations" do
     subject { build(:user) }
+
     # presence validations
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
@@ -38,4 +39,52 @@ RSpec.describe User, :type => :model do
       expect(build(:admin).admin).to be true
     end
   end
+
+  describe "methods" do
+    subject { create(:user) }
+
+    context "::find_by_credentials" do
+      it "finds user based on email and password" do
+        user = User.find_by_credentials(subject.email, 'password')
+        expect(user).to eq(subject)
+      end
+
+      it "finds user based on username and " do
+        user = User.find_by_credentials(subject.username, 'password')
+        expect(user).to eq(subject)
+      end
+    end
+
+    context "::generate_token" do
+      it "generates a random string" do
+        string1 = User.generate_token
+        string2 = User.generate_token
+        expect(string1).to_not eq(string2)
+      end
+    end
+
+    context "#reset_session_token!" do
+      it "resets the user's session token" do
+        old_session_token = subject.session_token
+        subject.reset_session_token!
+        expect(old_session_token).to_not eq(subject.session_token)
+      end
+    end
+
+    context "#password=" do
+      it "sets the user's password digest" do
+        user = User.new(password: 'password')
+        expect(user.password_digest).to_not be_nil
+      end
+    end
+
+    context "#is_password?" do
+      it "checks if password is the correct password" do
+        user = User.new(password: 'password')
+        expect(user.is_password?('password')).to be true
+      end
+    end
+
+  end
+
 end
