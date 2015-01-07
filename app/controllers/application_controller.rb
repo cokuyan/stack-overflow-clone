bc4963acf2cf34852736f8a2b7b4d37babdfeea1
Cihangir Okuyan <chiryoku@gmail.com>
2015-01-07 12:12:15 -0500

    Setup users controller and logging in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return nil unless session[:session_token]
+    user = User.find_by_session_token(session[:session_token])
+    @current_user ||= user
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def ensure_current_user
+    redirect_to login_url unless logged_in?
+  end
+
 end
