c1562d189f6385007d4857d205cb27de0f2a0d5f
Cihangir Okuyan <chiryoku@gmail.com>
2015-01-07 11:51:47 -0500

    Fill in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # will implement activation later
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome, #{@user.username}"
+      login!(@user)
+      redirect_to root_url
+    else
+      flash.now(:errors) = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Edited successfully"
+      redirect_to user_url(@user)
+    else
+      flash.now(:errors) = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "#{@user.username} removed successfully"
+    redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    # add activation_token?
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
