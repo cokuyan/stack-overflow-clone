cf68e953b6ca0324ca939ef4a2530e66990b001a
phil/jesus <phil/jesus@appacademy.com>
2015-01-08 11:17:36 -0500

    Send proper emails to allow user to activate self

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "notifications@stack-overflown.com"
+
+  def welcome_email(user)
+    @user = user
+    mail(to: user.email, subject: 'Welcome to Stack Overflown!')
+  end
 end
