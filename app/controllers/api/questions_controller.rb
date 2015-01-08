62469fadd9131606dd5ef83dd52399b7074f9440
phil/jesus <phil/jesus@appacademy.com>
2015-01-08 18:08:13 -0500

    Add backbone question new and show views and controller actions

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,4 +2,26 @@ class Api::QuestionsController < ApplicationController
   def index
     render json: Question.all
   end
+
+  def show
+    question = Question.find(params[:id])
+    question.view_count += 1
+    question.save!
+    render json: question
+  end
+
+  def create
+    question = current_user.questions.new(question_params)
+    if question.save
+      render json: question
+    else
+      render json: question.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108220447) do
+ActiveRecord::Schema.define(version: 20150108225823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20150108220447) do
     t.boolean  "answered",      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "answers_count"
+    t.integer  "answers_count", default: 0
     t.integer  "vote_count",    default: 0
   end
 
