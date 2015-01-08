442c26e5dff53dbc59b12815b4b1d36d77cd15d3
cokuyan <chiryoku@gmail.com>
2015-01-07 21:25:47 -0500

    Attempt at creating votes spec

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
   validates :user_id, :votable_id, :votable_type, null: false
   validates :vote_type, inclusion: ['up', 'down']
-  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
+  validates :user_id, uniqueness: { scope: :votable }
   # try with just votable
   belongs_to :user
   belongs_to :votable, polymorphic: true
