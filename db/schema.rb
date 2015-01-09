cf2086000deae4213d1ec0922262bfbefd6489e9
Cihangir Okuyan <chiryoku@gmail.com>
2015-01-09 17:55:55 -0500

    Add tag and tagging model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108225823) do
+ActiveRecord::Schema.define(version: 20150109224648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,24 @@ ActiveRecord::Schema.define(version: 20150108225823) do
   end
 
   add_index "questions", ["author_id"], name: "index_questions_on_author_id", using: :btree
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id",      null: false
+    t.integer  "question_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["tag_id", "question_id"], name: "index_taggings_on_tag_id_and_question_id", unique: true, using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "tag_name",    null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["tag_name"], name: "index_tags_on_tag_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username",                         null: false
