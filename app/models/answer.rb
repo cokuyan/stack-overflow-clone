050278fbf2ddc16893665c7e204eb8f765b04d72
Cihangir Okuyan <chiryoku@gmail.com>
2015-01-09 15:51:44 -0500

    Add voting and answer custom validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,7 @@ class Answer < ActiveRecord::Base
   validates :content, :author_id, :question_id, presence: true
   validates :accepted, inclusion: [true, false]
   validates :author_id, uniqueness: { scope: :question_id }
+  validate :cannot_answer_own_question
 
   belongs_to :author,
     class_name: 'User',
@@ -12,5 +13,13 @@ class Answer < ActiveRecord::Base
     counter_cache: true
 
   belongs_to :question, inverse_of: :answers, counter_cache: true
+
+  private
+
+  def cannot_answer_own_question
+    if self.author_id == self.question.author_id
+      errors.add(:author, "cannot answer own question")
+    end
+  end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,17 @@ class Vote < ActiveRecord::Base
   validates :user_id, :votable_id, :votable_type, presence: true
   validates :vote_type, inclusion: ['up', 'down']
   validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
-  # try with just votable <-- doesn't seem to work with specs
+  validate :cannot_vote_for_own_votable
+
   belongs_to :user
   belongs_to :votable, polymorphic: true
+
+  private
+
+  def cannot_vote_for_own_votable
+    if self.user_id == self.votable.author_id
+      errors.add(:user, "cannot vote for own #{self.votable_type}")
+    end
+  end
+
 end
